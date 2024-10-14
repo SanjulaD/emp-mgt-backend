b9bcdb0763045239c19ca6fcf6bc0797669acaa1
SanjulaD <desanjula99@gmail.com>
2024-10-14 11:44:29 +0530

    feat: update deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: '/graphql', graphql_path: '/graphql'
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphql', graphql_path: '/graphql'
+  end
+  
   post "/graphql", to: "graphql#execute"
   get "up" => "rails/health#show", as: :rails_health_check
 end
